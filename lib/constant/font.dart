72b47d65b409685fac6a9e77de24c46624c83b87
Barathsaraan <saraan.barath@gmail.com>
2024-09-14 06:14:48 +0530

    changesdone

--- lib/constant/font.dart
+++
@@ -1,8 +0,0 @@
-class AppFonts {
-  static const String gilroy = 'gilroy';
-  static const String lufga = 'lufga';
-  static const String Satoshi = 'Satoshi';
-  static const String Manrope = 'Manrope';
-
-
-}
